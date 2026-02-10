19329b61b36a171d788dd6a3770934716640b092
GitHub Action <action@github.com>
2026-02-10 22:50:29 +0100

    added missing widget components

---
+++ resources/js/components/landing/widgets/FaqWidget.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container class="py-8">
+    <h2 v-if="content.title" class="text-h4 text-center mb-8">{{ content.title }}</h2>
+    <v-expansion-panels variant="accordion">
+      <v-expansion-panel
+        v-for="(faq, index) in content.faqs"
+        :key="index"
+      >
+        <v-expansion-panel-title>
+          {{ faq.question }}
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          {{ faq.answer }}
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
+</template>
+
+<script setup>
+const props = defineProps({
+  content: {
+    type: Object,
+    default: () => ({
+      title: 'Frequently Asked Questions',
+      faqs: [
+        {
+          question: 'How do I get started?',
+          answer: 'Getting started is easy! Simply sign up for an account.',
+        },
+      ],
+    }),
+  },
+  config: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+</script>
---
+++ resources/js/components/landing/widgets/FeatureGridWidget.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container class="py-8">
+    <v-row>
+      <v-col
+        v-for="(feature, index) in content.features"
+        :key="index"
+        cols="12"
+        sm="6"
+        md="3"
+        class="text-center"
+      >
+        <v-icon :icon="feature.icon" size="48" color="primary" class="mb-4" />
+        <h3 class="text-h6 mb-2">{{ feature.title }}</h3>
+        <p class="text-body-2 text-grey">{{ feature.description }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+const props = defineProps({
+  content: {
+    type: Object,
+    default: () => ({
+      features: [
+        { icon: 'mdi-account-check-outline', title: 'Account Verification', description: 'Lorem ipsum dolor sit amet' },
+        { icon: 'mdi-lifebuoy', title: 'Dedicated Support', description: 'Lorem ipsum dolor sit amet' },
+        { icon: 'mdi-email-outline', title: 'Email Integration', description: 'Lorem ipsum dolor sit amet' },
+        { icon: 'mdi-clock-outline', title: 'Save Time', description: 'Lorem ipsum dolor sit amet' },
+      ],
+    }),
+  },
+  config: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+</script>
